014cbd48b3a85b798046a78b438933158e070322
kukuhyudhistiro <kukuh.yudhistiro@gmail.com>
2021-06-28 12:33:40 +0700

    Bahas reading item list dengan loop

---
+++ ekspedisi_readingList_smt2.py
@@ -0,0 +1,24 @@
+"""
+Created on Sat Jun 12 09:42:52 2021
+
+@author: x220
+"""
+"""
+Buat program untuk menghitung biaya total pengiriman barang di perusahaan Ekspedisi
+Lorena di Malang
+SOAL Aplikasi 6e hal 3
+"""
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("")
+print("==================================================")
+print(" DAFTAR MENU ")
+print("==================================================")
+print(" a = COCA COLA   Rp 10.000")
+print(" b = AQUA        Rp 5.000")
+print(" c = MIZONE      Rp 8.000")
+print(" d = ABC KOPI    Rp 3.000")
+print(" e = MOCACINO    Rp 15.000")
+print("==================================================")
+print("")
